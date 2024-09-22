98dbe89b110451f7fe016e1097cf2332a877b1b4
SkylerZhou <skylerzzc@gmail.com>
2024-09-22 16:03:21 -0400

    Isolated out SZ_adjust_network_to_remove_rejects func. Calculate correlation of all amp and lat for each patient.

---
+++ SZ_adjust_network_to_remove_rejects.m
@@ -0,0 +1,72 @@
+%% Aim: for the rejected signal, replace amplitude and latencies of N1 and N2 with NaN. 
+% Would be used during sanity checks (correlation between amplitude and latencies & correlation between amplitude and distance)
+function out = SZ_adjust_network_to_remove_rejects(out)
+
+
+% get num of electrodes for this patient 
+num_elecs = size(out.elecs, 2);
+
+
+% Pre-allocate n1_adj and n2_adj matrix with nan
+for row = 1:num_elecs
+    if size(out.elecs(row).avg, 1) >= 1
+        out.elecs(row).n1_adj = nan(num_elecs, 2); 
+        out.elecs(row).n2_adj = nan(num_elecs, 2); 
+    end
+end
+
+
+% for each patient's out file, loop over all electrodes 
+for ich = 1:num_elecs
+    
+    %% adj n1
+    % if the current elecs is not a stimulating elecs, skip it as it
+    % contains no data to be altered.
+    if isempty(out.elecs(ich).arts), continue; end
+        
+    % extract n1 
+    n1 = out.elecs(ich).N1(:,1:2);
+        
+    % replace the 0s with NaN in the new_version of N1 amp and latencies
+    for jch = 1:num_elecs
+        if n1(jch, 1) == 0 && n1(jch, 2) == 0 % if both amp and lat are zeros
+            n1(jch, 1) = NaN;
+            n1(jch, 2) = NaN;
+        end
+    end
+   
+    % exclude the n1 if the ccep signal are rejected during pipeline
+    % get the keep for the current stimulating electrode
+    keep_vector = out.rejection_details(1).reject.keep(ich,:);
+    % identify indices where keep_vector is NaN or 0
+    invalid_indices = isnan(keep_vector) | (keep_vector == 0);
+    % replace corresponding elements in n1 amplitude and latency with NaN
+    n1(invalid_indices, 1) = NaN;
+    n1(invalid_indices, 2) = NaN;
+        
+    % fill n1_adj with the adjusted n1 amp and lat
+    out.elecs(ich).n1_adj = n1;
+
+
+    %% adj n2 
+    % extract n1 
+    n2 = out.elecs(ich).N2(:,1:2);
+        
+    % replace the 0s with NaN in the new_version of N1 amp and latencies
+    for jch = 1:num_elecs
+        if n2(jch, 1) == 0 && n2(jch, 2) == 0 % if both amp and lat are zeros
+            n2(jch, 1) = NaN;
+            n2(jch, 2) = NaN;
+        end
+    end
+
+    % replace corresponding elements in n1 amplitude and latency with NaN
+    n2(invalid_indices, 1) = NaN;
+    n2(invalid_indices, 2) = NaN;
+        
+    % fill n1_adj with the adjusted n1 amp and lat
+    out.elecs(ich).n2_adj = n2;
+
+end
+
+
--- SZ_spearman_ampAndLat.m
+++ SZ_spearman_ampAndLat.m
@@ -1,7 +1,7 @@
 %% calcaulte spearman correlation between N1 amplitude and latency. 
 
 %% loop over patients
-num_patient = 10;
+num_patient = 55;
 ptT = readtable(['/Users/zhouzican/Documents/MATLAB/toolboxs/CCEP/pt_mat/','master_pt_list.xlsx']);
 patient_files = string(strcat(ptT.HUPID, '.mat'));
 patient_ids = string(ptT.HUPID(1:num_patient));
@@ -18,7 +18,7 @@ idx_patient = 1;
 
 for n = 1:num_patient
 
-    which_version = 'new_pipeline';
+    which_version = 'ori_pipeline';
     
     % obtain patient file 
     patient_file = fullfile('toolboxs', 'CCEP', 'ccep_result', which_version, patient_files(n));
@@ -29,6 +29,9 @@ for n = 1:num_patient
         out = temp.pt_out;
     end
 
+    % adjust amp and lat for n1&n2 to NaN if they are rejected in
+    % rejection_details
+    out = SZ_adjust_network_to_remove_rejects(out); 
     
     %% loop over elecs
     % to store correlations between the amplitude and latency of each electrode for
@@ -44,54 +47,16 @@ for n = 1:num_patient
     % --> should yield one correlation for each stimulating electrode 
     for ich = 1:num_elecs
         if isempty(out.elecs(ich).arts), continue; end
-        
-        % 1.0 extract n1 amplitude and latency 
-        n1_amp = out.elecs(ich).N1(:,1);
-        n1_lat = out.elecs(ich).N1(:,2);
-        
-        % 1.1 replace the 0s with NaN in the new_version of N1 amp and latencies
-        if strcmp(which_version, 'new_pipeline')
-            for jch = 1:num_elecs
-                if n1_amp(jch) == 0 && n1_lat(jch) == 0
-                    n1_amp(jch) = NaN;
-                    n1_lat(jch) = NaN;
-                end
-            end
-        end
 
-        % 1.2 exclude the n1_amp and n1_lat if the stim_resp are rejected 
-        % get the keep for the current stimulating electrode
-        keep_vector = out.rejection_details(1).reject.keep(ich,:);
-        % identify indices where keep_vector is NaN or 0
-        invalid_indices = isnan(keep_vector) | (keep_vector == 0);
-        % replace corresponding elements in n1_amp and n1_lat with NaN
-        n1_amp(invalid_indices) = NaN;
-        n1_lat(invalid_indices) = NaN;
-        
-        % 1.3 Calculate Spearman's correlation between N1 amplitude and latency
+        % Calculate Spearman's correlation between N1 amplitude and latency
+        n1_amp = out.elecs(ich).n1_adj(:,1);
+        n1_lat = out.elecs(ich).n1_adj(:,2);
         n1_corr = corr(n1_amp, n1_lat,'rows','pairwise','type','spearman'); % NaN values will be ignored during pariwise 
         n1_corr_vector(idx) = n1_corr;
         
-
-        % 2.0 repeat these steps for n2 to calculate spearman's correlation between n2 amplitude and latency
-        n2_amp = out.elecs(ich).N2(:,1);
-        n2_lat = out.elecs(ich).N2(:,2);
-        
-        % 2.1 replace the 0s with NaN in the new_version of N1 amp and latencies
-        if strcmp(which_version, 'new_pipeline')
-            for jch = 1:num_elecs
-                if n2_amp(jch) == 0 && n2_lat(jch) == 0
-                    n2_amp(jch) = NaN;
-                    n2_lat(jch) = NaN;
-                end
-            end
-        end
-
-        % 2.2 exclude the n2_amp and n2_lat if the stim_resp are rejected 
-        n2_amp(invalid_indices) = NaN;
-        n2_lat(invalid_indices) = NaN;
-        
-        % 2.3 Calculate Spearman's correlation between N2 amplitude and latency
+        % Calculate Spearman's correlation between N2 amplitude and latency
+        n2_amp = out.elecs(ich).n2_adj(:,1);
+        n2_lat = out.elecs(ich).n2_adj(:,2);
         n2_corr = corr(n2_amp, n2_lat,'rows','pairwise','type','spearman');
         n2_corr_vector(idx) = n2_corr;
     
@@ -115,7 +80,6 @@ disp(['p-value for the one-sample t-test on N1 correlations: ', num2str(p_n1)]);
 
 [h_n2, p_n2, ci_n2, stats_n2] = ttest(all_n2_corrs, 0, 'Tail', 'left'); 
 disp(['p-value for the one-sample t-test on N2 correlations: ', num2str(p_n2)]);
-
 
 
 
@@ -185,23 +149,27 @@ title(sprintf('Forest Plot of N2 Correlations (p-value: %.4e)', p_n2));
 
 grid on;
 hold off;
+%--------------------------------------------------------------------------
 
 
 
 
 
-
-%% correlation at patient level. 
+%--------------------------------------------------------------------------
+%% correlation of all electrodes at patient level. 
 % obtain patient out file 
-num_patient = 20;
+num_patient = 55;
 ptT = readtable(['/Users/zhouzican/Documents/MATLAB/toolboxs/CCEP/pt_mat/','master_pt_list.xlsx']);
 patient_files = string(strcat(ptT.HUPID, '.mat'));
-patient_ids = string(ptT.HUPID(num_patient));
+patient_ids = string(ptT.HUPID(1:num_patient));
+
+all_n1_corrs = nan(1, num_patient);
+all_n2_corrs = nan(1, num_patient);
 
 
 for n = 1:num_patient
     
-    which_version = 'new_pipeline';
+    which_version = 'ori_pipeline';
     
     % obtain patient file 
     patient_file = fullfile('toolboxs', 'CCEP', 'ccep_result', which_version, patient_files(n));
@@ -212,74 +180,109 @@ for n = 1:num_patient
         out = temp.pt_out;
     end
 
+    % adjust amp and lat for n1&n2 
+    out = SZ_adjust_network_to_remove_rejects(out); 
 
     % to store amp and lat across all electrodes for each patient 
-    %n1_ampLat_oneElecs = []; % To store N1 amp and lat pairs for each stimulating electrode
-    %n2_ampLat_oneElecs = []; % To store N2 amp and lat pairs ..
     num_elecs = size(out.elecs, 2);
     n1_ampLat_all = []; % To store N1 amp and lat pairs for all the stimulating electrode
     n2_ampLat_all = []; % To store N2 amp and lat pairs ..
-
     
+
     for ich = 1:num_elecs
         if isempty(out.elecs(ich).arts), continue; end
+        n1_ampLat = out.elecs(ich).n1_adj;
+        n2_ampLat = out.elecs(ich).n2_adj;
         
-        % 1.0 extract n1 amplitude and latency for all of the responding
-        % electrodes of the ich-th stimulating electrode --> size =
-        % num_elecs * 2
-        n1_ampLat = out.elecs(ich).N1(:,1:2);
-        
-        % 1.1 replace the 0s with NaN in the new_version of N1 amp and latencies
-        if strcmp(which_version, 'new_pipeline')
-            for jch = 1:num_elecs
-                if n1_ampLat(jch,1) == 0 && n1_ampLat(jch,2) == 0
-                    n1_ampLat(jch,1) = NaN;
-                    n1_ampLat(jch,2) = NaN;
-                end
-            end
-        end
-
-        % 1.2 exclude the n1_ampLat if the stim_resp are rejected 
-        % get the keep for the current stimulating electrode
-        keep_vector = out.rejection_details(1).reject.keep(ich,:);
-        % identify indices where keep_vector is NaN or 0
-        invalid_indices = isnan(keep_vector) | (keep_vector == 0);
-        % replace corresponding elements in n1_amp and n1_lat with NaN
-        n1_ampLat(invalid_indices) = NaN;
-        
-        % 1.3 store the processed n1_ampLat 
+        % store the processed n1_ampLat 
         n1_ampLat = transpose(n1_ampLat);
         n1_ampLat_all = [n1_ampLat_all, n1_ampLat];
 
-        % 2.0 extract n1 amplitude and latency 
-        n2_ampLat = out.elecs(ich).N2(:,1:2);
-        
-        % 2.1 replace the 0s with NaN in the new_version of N1 amp and latencies
-        if strcmp(which_version, 'new_pipeline')
-            for jch = 1:num_elecs
-                if n2_ampLat(jch,1) == 0 && n2_ampLat(jch,2) == 0
-                    n2_ampLat(jch,1) = NaN;
-                    n2_ampLat(jch,2) = NaN;
-                end
-            end
-        end
-
-        % 2.2 exclude the n2_ampLat if the stim_resp are rejected 
-        % replace corresponding elements in n1_amp and n1_lat with NaN
-        n2_ampLat(invalid_indices) = NaN;
-
-        % 2.3 store the processed n2_ampLat 
+        % store the processed n2_ampLat 
         n2_ampLat = transpose(n2_ampLat);
         n2_ampLat_all = [n2_ampLat_all, n2_ampLat];
     end
+
+
+    % calculate correlation between amplitude and latency all N1 and N2 
+    n1_amp = n1_ampLat_all(1,:)';
+    n1_lat = n1_ampLat_all(2,:)';
+    n1_corr = corr(n1_amp, n1_lat,'rows','pairwise','type','spearman'); % NaN values will be ignored during pariwise 
+
+    n2_amp = n2_ampLat_all(1,:)';
+    n2_lat = n2_ampLat_all(2,:)';
+    n2_corr = corr(n2_amp, n2_lat,'rows','pairwise','type','spearman'); % NaN values will be ignored during pariwise 
+
+    all_n1_corrs(n) = n1_corr;
+    all_n2_corrs(n) = n2_corr;
 end
 
 
+%% if the correlation is significantly negative
+[h_n1, p_n1, ci_n1, stats_n1] = ttest(all_n1_corrs, 0, 'Tail', 'left'); 
+disp(['p-value for the one-sample t-test on N1 correlations: ', num2str(p_n1)]);
+
+[h_n2, p_n2, ci_n2, stats_n2] = ttest(all_n2_corrs, 0, 'Tail', 'left'); 
+disp(['p-value for the one-sample t-test on N2 correlations: ', num2str(p_n2)]);
+
+
+%% forest plot
+figure;
+tiledlayout(1,2);
+
+%% Plot for N1
+nexttile;
+hold on;
+
+for i = 1:num_patient   
+    % Plot a marker at the mean
+    plot(all_n1_corrs(i), i, 'bo', 'MarkerFaceColor', [0 0.4470 0.7410]);
+end
+
+% Draw vertical red line at x=0
+xline(0, ':', 'LineWidth', 1);
+
+% Set axes properties
+xlim([-1, 1]);
+ylim([0.5, num_patient + 0.5]);
+set(gca, 'YTick', 1:num_patient, 'YTickLabel', patient_ids);
+ylabel('Patient ID');
+xlabel('Spearman Correlation');
+title(sprintf('Forest Plot of N1 Correlations (p-value: %.4e)', p_n1));
+
+grid on;
+hold off;
+
+%% Plot for N2
+nexttile;
+hold on;
+
+for i = 1:num_patient
+    % Plot a marker at the mean
+    plot(all_n2_corrs(i), i, 'bo', 'MarkerFaceColor', [0 0.4470 0.7410]);
+end
+
+% Draw vertical red line at x=0
+xline(0, ':', 'LineWidth', 1);
+
+% Set axes properties
+xlim([-1, 1]);
+ylim([0.5, num_patient + 0.5]);
+set(gca, 'YTick', 1:num_patient, 'YTickLabel', patient_ids);
+ylabel('Patient ID');
+xlabel('Spearman Correlation');
+title(sprintf('Forest Plot of N2 Correlations (p-value: %.4e)', p_n2));
+
+grid on;
+hold off;
+%--------------------------------------------------------------------------
 
 
 
 
-%% amplitude and latencty scatter plot in patient level. 
+
+%--------------------------------------------------------------------------
+%% scatter plot of amp and lat in patient level. 
 % obtain patient out file 
 num_patient = 20;
 ptT = readtable(['/Users/zhouzican/Documents/MATLAB/toolboxs/CCEP/pt_mat/','master_pt_list.xlsx']);
@@ -300,6 +303,8 @@ for n = num_patient:num_patient
         out = temp.pt_out;
     end
 
+    % adjust amp and lat for n1&n2 
+    out = SZ_adjust_network_to_remove_rejects(out); 
 
     % to store amp and lat across all electrodes for each patient 
     %n1_ampLat_oneElecs = []; % To store N1 amp and lat pairs for each stimulating electrode
@@ -311,52 +316,14 @@ for n = num_patient:num_patient
     
     for ich = 1:num_elecs
         if isempty(out.elecs(ich).arts), continue; end
+        n1_ampLat = out.elecs(ich).n1_adj;
+        n2_ampLat = out.elecs(ich).n2_adj;
         
-        % 1.0 extract n1 amplitude and latency for all of the responding
-        % electrodes of the ich-th stimulating electrode --> size =
-        % num_elecs * 2
-        n1_ampLat = out.elecs(ich).N1(:,1:2);
-        
-        % 1.1 replace the 0s with NaN in the new_version of N1 amp and latencies
-        if strcmp(which_version, 'new_pipeline')
-            for jch = 1:num_elecs
-                if n1_ampLat(jch,1) == 0 && n1_ampLat(jch,2) == 0
-                    n1_ampLat(jch,1) = NaN;
-                    n1_ampLat(jch,2) = NaN;
-                end
-            end
-        end
-
-        % 1.2 exclude the n1_ampLat if the stim_resp are rejected 
-        % get the keep for the current stimulating electrode
-        keep_vector = out.rejection_details(1).reject.keep(ich,:);
-        % identify indices where keep_vector is NaN or 0
-        invalid_indices = isnan(keep_vector) | (keep_vector == 0);
-        % replace corresponding elements in n1_amp and n1_lat with NaN
-        n1_ampLat(invalid_indices) = NaN;
-        
-        % 1.3 store the processed n1_ampLat 
+        % store the processed n1_ampLat 
         n1_ampLat = transpose(n1_ampLat);
         n1_ampLat_all = [n1_ampLat_all, n1_ampLat];
-
-        % 2.0 extract n1 amplitude and latency 
-        n2_ampLat = out.elecs(ich).N2(:,1:2);
-        
-        % 2.1 replace the 0s with NaN in the new_version of N1 amp and latencies
-        if strcmp(which_version, 'new_pipeline')
-            for jch = 1:num_elecs
-                if n2_ampLat(jch,1) == 0 && n2_ampLat(jch,2) == 0
-                    n2_ampLat(jch,1) = NaN;
-                    n2_ampLat(jch,2) = NaN;
-                end
-            end
-        end
-
-        % 2.2 exclude the n2_ampLat if the stim_resp are rejected 
-        % replace corresponding elements in n1_amp and n1_lat with NaN
-        n2_ampLat(invalid_indices) = NaN;
-
-        % 2.3 store the processed n2_ampLat 
+     
+        % store the processed n2_ampLat 
         n2_ampLat = transpose(n2_ampLat);
         n2_ampLat_all = [n2_ampLat_all, n2_ampLat];
     end
