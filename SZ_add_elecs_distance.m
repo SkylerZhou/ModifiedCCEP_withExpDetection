12e2108615fd5f35b36c532b56414a6fada2fe2d
SkylerZhou <skylerzzc@gmail.com>
2024-10-02 00:08:54 -0400

    updated on SZ_add_elecs_distance func

--- SZ_add_elecs_distance.m
+++ SZ_add_elecs_distance.m
@@ -1,12 +1,14 @@
 %% this function calculate elucidean distance betwwen electrodes and store the distance matrix as elecs_distance in out.other?
 function out = SZ_add_elecs_distance(out)
 
+
 % mount to remote server
 locations = cceps_files;
 mount_dir = locations.mount_dir;
-
 patient_id = out.name;
 coordinates_filename = string(strcat(patient_id, '.csv'));
+
+
 
 % read the metadata file containing the electrode from the mounted dir
 % names and xyz coordinates 
@@ -20,6 +22,7 @@ catch ME
 end
 
 
+
 % check whether the elecs number and names in the coordinate_file and
 % patient_file match; adjust electrodes and their corresponding xyz
 % coordinates as nan if not matched
@@ -28,15 +31,23 @@ coordinates_file.name = strrep(coordinates_file.name, '''', '');
 patient_elecs.name = string(patient_elecs.name);
 coordinates_file.name = string(coordinates_file.name);
 
-test = outerjoin(patient_elecs, coordinates_file, 'Keys', 'name', 'Type', 'left');
+% have to re-order the output after outerjoin because outerjoin
+% automatically sort by number 
+[coor_file, rowsidx_left] = outerjoin(patient_elecs, coordinates_file, 'Keys', 'name', 'Type', 'left');
+[~, sortinds] = sort(rowsidx_left);
+coor_file = coor_file(sortinds,:);
 
-% keep only the xyz in coordinate_file to run pdist2
+% keep only the xyz in coor_file to run pdist2
+coor_file(:,1:2) = [];
+
+
 
 % compute the pairwise euclidean distances between electrodes
-%elecs_dist = pdist2(elec_coords, elec_coords, 'euclidean');
-%out.other.elecs_dist = elecs_dist;
+elecs_dist = pdist2(table2array(coor_file), table2array(coor_file), 'euclidean');
+
+% create elecs_dist matrix under out.other
+out.other.elecs_dist = elecs_dist;
 
 
-% create elecs_dist matrix under out.other as a num_elecs * num_elecs
-% matrix
+
     
