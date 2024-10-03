29582f42c40faf6d061338a699bb86401ee3ce74
SkylerZhou <skylerzzc@gmail.com>
2024-10-03 12:40:26 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # ModifiedCCEP_withExpDetection
 
-## Updated on three aspects of https://github.com/RudyWh/Modified_CCEP_Detection to fit our cohort:
+# 1. to_run_newPipeline
+# Updated on three aspects of https://github.com/RudyWh/Modified_CCEP_Detection to fit our cohort:
 1. Added an **exp** component in AA_new_build_network.m, which utilize Curve Fitting Toolbox to fit an *standardized & low-pass filtered* exponential function to approximately 0-0.3 second of the recording. Reject the signal if its goodness-of-fit is bigger than 0.6. 
 2. Lower the **thresh_amp** from 0.65 to 0.6.
 3. Delete the **deriv** component in AA_alternative_filtering.m and AA_new_build_network.m.
+
+# 2. validation_on_newPipeline
+# Supplementary files for graphing and checking whether expoential waves and stim artifacts are correctly rejected. 
+1. SZ_validateExp.m randomly select 25 signals that were rejected due to the exp component. The signals were mapped out so that we could visually examine if they were actually of expoential shape and thus correctly rejected. 
+2. Stim artifacts noticeably increase among the 25 randomly selected kept figures after processing using the new pipleine. To aid in checking if these artifacts were retained in both original and new pipeline, SZ_validateStimArtifact.m were built to compare them in both versions of the pipeline using the same set of coordinates. 
+
+# 3. sanity_checks
+# Conducted pair-wise comparison between N1&N2 amplitudes and latencies, as well as electrode distances as a simple sanity check on the new pipeline. 
+1. SZ_adjust_network_to_remove_rejects.m convert amplitude and latency of the N1&N2 that were rejected to NaN.
+2. SZ_add_elecs_distance.m adds a electrode distance matrix. 
--- SZ_rankMat.m
+++ SZ_rankMat.m
@@ -6,44 +6,52 @@
 % 3). The ranking will be NaN if no latencies are recorded (or if they are
 % rejected) 
 
-% specify directories
-num_patient = 5;
-ptT = readtable(['/Users/zhouzican/Documents/MATLAB/toolboxs/CCEP/pt_mat/','master_pt_list.xlsx']);
+%% prep
+locations = cceps_files;
+data_folder = locations.data_folder;
+ptT = readtable([data_folder,'master_pt_list.xlsx']);
+
+num_patient = height(ptT);
 patient_files = string(strcat(ptT.HUPID, '.mat'));
+%
 
 
-% loop over patients
-for n = 5:num_patient
+%% loop over patients
+for n = 5:5
 
-    % load patient data 
+    % load patient out file  
     new_patient_file = fullfile('toolboxs', 'CCEP', 'ccep_result', 'new_pipeline', patient_files(n));
     temp = load(new_patient_file);
     out = temp.new_out;
+    % adjust amp and lat for n1&n2 to NaN if they are rejected 
+    out = SZ_adjust_network_to_remove_rejects(out); 
 
     % build matrix to store the rankings 
     num_elecs = size(out.elecs, 2);
     n1_rank_mat = NaN(num_elecs, num_elecs);
     n2_rank_mat = NaN(num_elecs, num_elecs);
-
-    % extract latencies of all responding electrode to a single stimulating
-    % electrode
     idx_elecs = 1;
+    % 
+
+    % loop over all elecs of this patient 
+    % extract latencies of all responding elecs of a stim electrode
     for ich = 1:num_elecs
         % if the current ich-th elecs is not stimulating, skip it and leave
         % its row in rank_mat all NaN.
         if isempty(out.elecs(ich).arts), continue; end
 
         %% for N1
-        n1_lat = out.elecs(ich).N1(:,2);
-        % process to filter out the 0s and the ones rejected 
         % rank according to their latencies
-        % store as the idx_elecs row in n1_rank_mat 
+        n1_lat = out.elecs(ich).n1_adj(:,2);
+        rank_n1_lat = tiedrank(n1_lat, 'nanflag', 'omitnan');
+
+        % store as the idx_elecs row in n1_rank_mat
+        n1_rank_mat(ich,:) = rank_n1_lat;
 
 
     end
 
 end
-
 
 
 %% compare two rankMat using Brain Connectivity Toolbox 
