697aa1ee914caeab9152d211d4af963d12f756df
SkylerZhou <skylerzzc@gmail.com>
2024-10-07 21:04:07 -0400

    added ccep_files.m and HUPID_RID_conversion code in python

---
+++ cceps_files.m
@@ -0,0 +1,17 @@
+function locations = cceps_files
+
+locations.pwfile = "/Users/zhouzican/Documents/MATLAB/master_thesis/ieeg_login/sky_ieeglogin.bin";
+locations.ieeg_folder = '/Users/zhouzican/Documents/MATLAB/master_thesis/ieeg-matlab-1.14.60/IEEGToolbox/';
+locations.loginname = 'skylerz';
+locations.script_folder = '/Users/zhouzican/Documents/MATLAB/master_thesis/CCEP/ccep_script/';
+locations.results_folder = '/Users/zhouzican/Documents/MATLAB/master_thesis/CCEP/ccep_result/';
+locations.data_folder = '/Users/zhouzican/Documents/MATLAB/master_thesis/CCEP/pt_mat/';
+locations.patientNewOut_dir = '/Users/zhouzican/Documents/MATLAB/master_thesis/CCEP/ccep_result/new_pipeline';
+locations.patientOriOut_dir = '/Users/zhouzican/Documents/MATLAB/master_thesis/CCEP/ccep_result/ori_pipeline';
+
+% files containing electrodes distance info
+locations.mount_dir = '/Volumes/USERS/skylerz/';
+%locations.coord_folder_dir1 = '/mnt/leif/littlab/data/Human_Data/CNT_iEEG_BIDS/';
+%locations.coord_folder_dir2 = '/mnt/leif/littlab/data/Human_Data/recon/BIDS_penn/';
+%locations.coord_file_dir = '/derivatives/ieeg_recon/module3/';
+end
